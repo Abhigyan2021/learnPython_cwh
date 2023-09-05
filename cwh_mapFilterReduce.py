9c92c492d747fffbf344b4ef6c88528ee4fd2941
Abhigyan2021 <aadeshabhigyan09@gmail.com>
2023-09-05 08:17:11 +0530

    Updated Filter Function in cwh_mapFilterReduce.py

--- cwh_mapFilterReduce.py
+++ cwh_mapFilterReduce.py
@@ -2,6 +2,9 @@
 ## map function 
 # It is used to convert perform on every item of a list through a function
 
+#NOTE: map, filter and reduce are higher order functions
+# Higher Order Functions are those functions which can take other functions as arguments
+
 
 def cube(x):
     return x*x*x
@@ -29,4 +32,26 @@ print(newl)
 
 
 ## FILTER FUNCTION
+#It filters out elements of a list based upon a function
+
+
+def filter_function(a):
+    return a > 4
+
+newnewl = list(filter(filter_function, l))
+print(newnewl)
+
+newnewl = list(filter(lambda a: a>=2, l))
+print(newnewl)
+
+
+##REDUCE FUNCTION
+#It performs an operation defined in a function upon every element in list and outputs final value
+
+#NOTE: reduce function must be imported
+
+
+from functools import reduce
+
+
 
