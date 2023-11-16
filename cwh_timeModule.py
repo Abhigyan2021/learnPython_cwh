5152bb1852efab349972f038168834fd73c03832
Abhigyan2021 <aadeshabhigyan09@gmail.com>
2023-11-16 20:22:39 +0530

    Command Line Utiliteis

--- cwh_timeModule.py
+++ cwh_timeModule.py
@@ -6,34 +6,34 @@ import time
 
 ## time() method
 
-# def usingWhile():
-#     i = 0
-#     while(i < 5000):
-#         i = i + 1
-#         print(i)
+def usingWhile():
+    i = 0
+    while(i < 5000):
+        i = i + 1
+        print(i)
 
-# def usingFor():
-#     for i in range(5000):
-#         print(i)
+def usingFor():
+    for i in range(5000):
+        print(i)
 
-# init = time.time()
-# usingWhile()
-# t1 = time.time() - init
+init = time.time()
+usingWhile()
+t1 = time.time() - init
 
-# init = time.time()
-# usingFor()
-# t2 = time.time() - init
+init = time.time()
+usingFor()
+t2 = time.time() - init
 
-# print(f"Time for Running While Loop is : {t1}")
-# print(f"Time for Running For LOOP is : {t2}")
+print(f"Time for Running While Loop is : {t1}")
+print(f"Time for Running For LOOP is : {t2}")
 
 
 ## sleep() method
 
-# print("\n")
-# print(4)
-# time.sleep(20)
-# print("This is printed after waiting 20 seconds")
+print("\n")
+print(4)
+time.sleep(20)
+print("This is printed after waiting 20 seconds")
 
 ## localtime() method
 
