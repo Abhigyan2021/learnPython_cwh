62f76e75f233694ee51d1dfc221c73dfe6ff4758
Abhigyan2021 <aadeshabhigyan09@gmail.com>
2023-11-16 19:40:41 +0530

    Time Module

--- cwh_timeModule.py
+++ cwh_timeModule.py
@@ -1,23 +1,47 @@
+
+#Time Module in Python
+
 import time
 
-def usingWhile():
-    i = 0
-    while(i < 5000):
-        i = i + 1
-        print(i)
 
-def usingFor():
-    for i in range(5000):
-        print(i)
+## time() method
 
-init = time.time()
-usingWhile()
-t1 = time.time() - init
+# def usingWhile():
+#     i = 0
+#     while(i < 5000):
+#         i = i + 1
+#         print(i)
 
-init = time.time()
-usingFor()
-t2 = time.time() - init
+# def usingFor():
+#     for i in range(5000):
+#         print(i)
 
-print(f"Time for Running While Loop is : {t1}")
-print(f"Time for Running For LOOP is : {t2}")
+# init = time.time()
+# usingWhile()
+# t1 = time.time() - init
+
+# init = time.time()
+# usingFor()
+# t2 = time.time() - init
+
+# print(f"Time for Running While Loop is : {t1}")
+# print(f"Time for Running For LOOP is : {t2}")
+
+
+## sleep() method
+
+# print("\n")
+# print(4)
+# time.sleep(20)
+# print("This is printed after waiting 20 seconds")
+
+## localtime() method
+
+t = time.localtime()
+
+## strftime() method
+
+formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
+
+print(formatted_time)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,20 @@
-lst = [i*i for i in range(4)]
-# print("list is",lst)
-# lst = [i*i for i in range(10) if i%2==0 ]
-# print("New list is",lst)
+def usingWhile():
+#     i = 0
+#     while(i < 5000):
+#         i = i + 1
+#         print(i)
+
+# def usingFor():
+#     for i in range(5000):
+#         print(i)
+
+# init = time.time()
+# usingWhile()
+# t1 = time.time() - init
+
+# init = time.time()
+# usingFor()
+# t2 = time.time() - init
+
+# print(f"Time for Running While Loop is : {t1}")
+# print(f"Time for Running For LOOP is : {t2}")
