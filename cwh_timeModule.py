f85f11ce83509654927a5abf96b382f7cc4c2266
Abhigyan2021 <aadeshabhigyan09@gmail.com>
2023-11-16 13:35:01 +0530

    Time Module in Python

---
+++ cwh_timeModule.py
@@ -0,0 +1,23 @@
+import time
+
+def usingWhile():
+    i = 0
+    while(i < 5000):
+        i = i + 1
+        print(i)
+
+def usingFor():
+    for i in range(5000):
+        print(i)
+
+init = time.time()
+usingWhile()
+t1 = time.time() - init
+
+init = time.time()
+usingFor()
+t2 = time.time() - init
+
+print(f"Time for Running While Loop is : {t1}")
+print(f"Time for Running For LOOP is : {t2}")
+
