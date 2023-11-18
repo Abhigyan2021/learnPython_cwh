5aa4d6afed7ad86dfb4dd9c9c51f4f9998bec901
Abhigyan2021 <aadeshabhigyan09@gmail.com>
2023-11-18 18:29:11 +0530

    CmdUtility1.py modified

--- cmdUtility1.py
+++ cmdUtility1.py
@@ -14,3 +14,26 @@ except ImportError as e:
 keyword = sys.argv[1] #Get the keyword that is to be searched for
 path = " ".join(sys.argv[2:]) #Get the path where to search for the keyword
 
+filesMatched = 0 # Initialize filesMatched as 0
+
+#Function to search the keyword in a given directory
+def search_files(keyword, path):
+    filesFound = filesMatched
+    for root, dirs, files in os.walk(path): # use os.walk(path) to loop through the given directory  and get the root, dirs, and files
+        for file in files : # Loop through all the files in the given directory
+            if keyword in file: # Check if keyword is in filename
+                filesFound += 1 # Counter to see how many files matched
+                print(" " + root + "\\" + str(file) + "\n") # Print out the full file path for every matching file
+                if filesFound == 0 : #check if no files matched the given keyword and print the same
+                    print("No matching files in the given directory! Try another directory!")
+
+    # If 1 or more files are matched then print the same.
+    print(f"No. of files matched : {filesFound}")
+
+try:
+    search_files(keyword, path) # Call the function
+# Error and Exception Handling
+except FileNotFoundError as e:
+    print("Error: FileNotFoundError Occurred! Make sure you entered the correct path and entered it correctly.")
+except Exception as e:
+    print(f"Error: Some Error occured! \nDetails: {e}")
